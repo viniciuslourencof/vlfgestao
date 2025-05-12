e765c3f7f84d21f47619caeda9a10926097ff0ee
unknown <lourencovinicius00@gmail.com>
2025-05-12 16:58:37 -0300

    adicionado inserção de pedidos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { SidebarNav } from "./components/sidebar-nav";
 import { Header } from "./components/header";
-import { Cart } from "./components/cart";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { ProdutosViewPage } from "./pages/ProdutosViewPage";
@@ -9,25 +8,12 @@ import { CategoriasViewPage } from "./pages/CategoriasViewPage";
 import { SearchProvider } from "@/components/search-provider"; // ajuste o caminho se necessário
 import { LoginPage } from "./pages/LoginPage"; // Página de login
 
-interface CarrinhoItem {
-  produto_id: number;
-  dsc_produto: string;
-  preco_venda1: number;
-  quantidade: number;
-}
-
 // App.tsx
-export function App() {
-  const [carrinho, setCarrinho] = useState<CarrinhoItem[]>([]);
-  const [minimized, setMinimized] = useState(true);
+export function App() {  
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [sidebarOpen, setSidebarOpen] = useState(false); 
 
   const navigate = useNavigate();
-
-  const handleRemoveItem = (index: number) => {
-    setCarrinho((prev) => prev.filter((_, i) => i !== index));
-  };
 
   useEffect(() => {
     const usuarioId = localStorage.getItem("usuario_id");
@@ -62,13 +48,7 @@ export function App() {
                       element={<CategoriasViewPage />}
                     />
                   </Routes>
-                </main>
-                <Cart
-                  carrinho={carrinho}
-                  onRemoveItem={handleRemoveItem}
-                  minimized={minimized}
-                  setMinimized={setMinimized}
-                />
+                </main>                
               </div>
             </div>
           </>
