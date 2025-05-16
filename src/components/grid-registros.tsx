715dc2635c85e9d925bddda2d02a9112c603154d
unknown <lourencovinicius00@gmail.com>
2025-05-16 16:19:17 -0300

    * Reescrito o grid das telas em um arquivo só pra ser reutilizavel

--- src/components/grid-registros.tsx
+++ src/components/grid-registros.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { AgGridReact } from "ag-grid-react";
 import type {
